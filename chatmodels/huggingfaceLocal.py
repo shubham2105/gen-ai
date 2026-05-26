f75a99252fe99654754a4ecaac8ceb510d5ccb38
Shubham Dhole <shubhamdhole@Shubhams-MacBook-Pro.local>
2026-05-26 17:23:00 +0530

    Used HuggingFacePipeline and ChatHuggingFace to integate TinyLLama Chat model locally for text-generation

---
+++ chatmodels/huggingfaceLocal.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        max_new_tokens=512,
+        do_sample=False,
+        repetition_penalty=1.03,
+        return_full_text=False,
+    ),
+)
+
+model = ChatHuggingFace(llm = llm)
+response = model.invoke("What is Machine Learning?")
+print(response.content)
