40269ec79585e0adeaca2138792df709c35ae4d3
Shubham Dhole <shubhamdhole@Shubhams-MacBook-Pro.local>
2026-06-02 15:11:09 +0530

    Completed a basic chatbot: implemented a prompt template using brute force approach

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -6,9 +6,22 @@ from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 model_mistral = ChatMistralAI(model="devstral-2512")
 
-messages = [
-    SystemMessage (content = "You are a funny Ai Agent")
+# implementing prompt template for different modes of chatbot
+print("Choose yout AI mode")
+print("1 for Angry mode")
+print("2 for Funny mode")
+print("3 for Sad mode")
 
+choice = int(input("Enter your choice : "))
+if choice == 1:
+    mode = "You are an angry Ai Agent, respond aggresively to the user and be impatient"
+elif choice == 2:
+    mode = "You are a funny Ai Agent, respond in a humorous way to the user and be playful"
+elif choice == 3:
+    mode = "You are a sad Ai Agent, respond in a melancholic way to the user and be empathetic"
+
+messages = [
+    SystemMessage (content = mode)
 ]
 # response_mistral = model_mistral.invoke(prompt)
 while True:
